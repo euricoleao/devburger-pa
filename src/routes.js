312dd2231e2f375c752bdb0d4fa36be895072c3c
unknown <euricobig@gmail.com>
2024-11-02 23:51:27 -0300

    alteração rote

--- src/routes.js
+++ src/routes.js
@@ -14,9 +14,9 @@ const routes = new Router();
 
 const upload = multer(multerConfig);
 
-routes.get('/', (req, res) => {
+/*routes.get('/', (req, res) => {
   return res.json({message: 'Hello to my first API '})
-})
+})*/
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
